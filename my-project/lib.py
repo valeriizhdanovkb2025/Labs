48691b65f4c401b5f2cf92563974c4e1b3b9c5e4
valeriizhdanovkb2025 <valerii.zhdanov.kb.2025@lpnu.ua>
2026-09-22 21:41:24 +0300

    Add test module

---
+++ my-project/lib.py
@@ -0,0 +1,3 @@
+def greet(name: str) -> str:
+    return f"Hello, {name}! Welcome to DevOps Lab 1."
+#feature branch
---
+++ my-project/main.py
@@ -0,0 +1,8 @@
+from lib import greet
+
+def main():
+    message = greet("Student")
+    print(message)
+
+if __name__ == "__main__":
+    main()
